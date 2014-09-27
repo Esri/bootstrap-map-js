1db2d200b65290293fc6e46e85de420f04f4e918
Ahjung Kim <ak@macintosh.gateway.pace.com>
2014-09-26 20:07:03 -0400

    Local change lost, local change found
    Money lost, something lost. Honor lost, much lost. Courage lost,
    everything lost-better you were never born. Johann Wolfgang von Goethe
    (1749 - 1832).

---
+++ templates/js/map.js
@@ -0,0 +1,139 @@
+/**
+ * Created by ak on 9/26/14.
+ */
+/**
+ * Created by Ahjung.Kim on 9/4/2014.
+ */
+require([
+        "esri/map",
+        "dojo/dom",
+        "application/bootstrapmap",
+        "esri/layers/ArcGISTiledMapServiceLayer",
+        "esri/layers/FeatureLayer",
+
+        "esri/dijit/OverviewMap",
+        "esri/dijit/HomeButton",
+        "esri/dijit/LocateButton",
+        "esri/dijit/Geocoder",
+        "esri/dijit/Measurement",
+        "esri/dijit/Scalebar",
+        "esri/dijit/Popup",
+        "esri/dijit/PopupTemplate",
+
+        "dojo/domReady!"
+    ],
+    function(Map, Scalebar, BootstrapMap, FeatureLayer, ArcGISTiledMapServiceLayer, dom, HomeButton, LocateButton, Geocoder,
+             OverviewMap, Measurement, Popup) {
+        // Get a reference to the ArcGIS Map class
+        var map = BootstrapMap.create("mapDiv", {
+            basemap: "topo",
+            center: [-85.724, 37.593],
+            zoom: 7,
+            scrollWheelZoom: true,
+            logo: false,
+            nav: false,
+            sliderPosition: "top-right"
+//            infoWindow: popup
+        });
+
+        /*var KYBase = new ArcGISTiledMapServiceLayer("http://maps.kytc.ky.gov/arcgis/rest/services/BaseMap/KYTCBaseMap/MapServer");
+         map.addLayer(KYBase);*/
+
+        // Add overview map
+        var overviewMapDijit = new esri.dijit.OverviewMap({
+            map: map,
+            attachTo: "bottom-right",
+            height: 150,
+            width: 200,
+            visible: true,
+            opacity: 0.4,
+            expandFactor: 3.0
+        });
+        overviewMapDijit.startup();
+
+//        var measurement = new esri.dijit.Measurement({
+//            map:map
+//        },dojo.byId("measurement"));
+//        measurement.startup();
+
+        var home = new esri.dijit.HomeButton({
+            map: map
+        }, "HomeButton");
+        home.startup();
+
+
+        // Add locate button
+        var geoLocate = new esri.dijit.LocateButton({
+            map: map,
+            scale: null
+        }, "LocateButton");
+        geoLocate.startup();
+
+//        var scalebar = new Scalebar({
+//            map: map,
+//            scalebarUnit: "dual"
+//        });
+
+        // Add geocoder
+        var geocoder2 = new esri.dijit.Geocoder({
+            map: map,
+            autoComplete: true,
+            arcgisGeocoder: {
+                name: "Esri World Geocoder"
+            }
+        }, "search");
+        geocoder2.startup();
+
+        $(document).ready(function() {
+            $("#basemapList li").click(function(e) {
+                switch (e.target.text) {
+                    case "Streets":
+                        map.setBasemap("streets");
+                        break;
+                    case "Imagery":
+                        map.setBasemap("hybrid");
+                        break;
+                    case "National Geographic":
+                        map.setBasemap("national-geographic");
+                        break;
+                    case "Topographic":
+                        map.setBasemap("topo");
+                        break;
+                    case "Gray":
+                        map.setBasemap("gray");
+                        break;
+                    case "Open Street Map":
+                        map.setBasemap("osm");
+                        break;
+                    case "KYTC Basemap":
+                        alert("lol");
+                        break;
+                }
+            });
+        });
+
+        // Add DGN basemap
+        var DGNBasemap  = new esri.layers.ArcGISTiledMapServiceLayer("http://kygisserver.ky.gov/arcgis/rest/services/WGS84WM_Services/Ky_TCM_Base_WGS84WM/MapServer");
+
+        // Add layers
+        var syp1 = new esri.layers.FeatureLayer("http://maps.kytc.ky.gov/arcgis/rest/services/Apps/ActiveHighwayPlan/MapServer/1", {
+            mode: esri.layers.FeatureLayer.MODE_ONDEMAND,
+            //infoTemplate: popupTemplate,
+            outFields: ["*"]
+        });
+        var syp2 = new esri.layers.FeatureLayer("http://maps.kytc.ky.gov/arcgis/rest/services/Apps/ActiveHighwayPlan/MapServer/2", {
+            mode: esri.layers.FeatureLayer.MODE_ONDEMAND
+        });
+        var syp3 = new esri.layers.FeatureLayer("http://maps.kytc.ky.gov/arcgis/rest/services/Apps/ActiveHighwayPlan/MapServer/3", {
+            mode: esri.layers.FeatureLayer.MODE_ONDEMAND
+        });
+        var syp4 = new esri.layers.FeatureLayer("http://maps.kytc.ky.gov/arcgis/rest/services/Apps/ActiveHighwayPlan/MapServer/4", {
+            mode: esri.layers.FeatureLayer.MODE_ONDEMAND
+        });
+        map.addLayer(DGNBasemap);
+        map.addLayer(syp1);
+        map.addLayer(syp2);
+        map.addLayer(syp3);
+        map.addLayer(syp4);
+
+    });
